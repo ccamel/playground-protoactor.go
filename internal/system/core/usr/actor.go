e8d657bb0e097351770c25b222daeb4a77b57add
Chris Camel <camel.christophe@gmail.com>
2020-11-29 21:38:14 +0100

    :construction: add booklend actor startup

--- internal/system/core/usr/actor.go
+++ internal/system/core/usr/actor.go
@@ -14,7 +14,13 @@
 package usr
 
 import (
+	"time"
+
 	"github.com/AsynkronIT/protoactor-go/actor"
+	"github.com/ccamel/playground-protoactor.go/internal/actor/booklend"
+	"github.com/golang/protobuf/ptypes"
+	"github.com/rs/zerolog/log"
+	"google.golang.org/genproto/googleapis/rpc/code"
 )
 
 type Actor struct {
@@ -23,6 +29,63 @@ type Actor struct {
 func (state *Actor) Receive(context actor.Context) {
 	switch context.Message().(type) {
 	case *actor.Started:
+		pid, _ := context.SpawnNamed(booklend.NewBookCommandHandler(), "book_lend")
+
+		bookID := "207b6be6-a7e4-4cc7-a692-b51e79de0460" // uuid.New().String()
+
+		res, err := context.RequestFuture(pid, &booklend.RegisterBook{
+			BookId: bookID,
+			Title:  "The Lord of the Rings",
+			Isbn:   "0-618-15396-9",
+		}, 5*time.Second).Result()
+
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to create book")
+			return
+		}
+
+		if res.(*booklend.CommandStatus).Code != code.Code_OK {
+			log.Warn().Interface("event", res).Msgf("error")
+			return
+		}
+
+		log.Info().Interface("event", res).Msgf("ok")
+
+		res, err = context.RequestFuture(pid, &booklend.LendBook{
+			BookId:           bookID,
+			Borrower:         "John Doe",
+			Date:             ptypes.TimestampNow(),
+			ExpectedDuration: ptypes.DurationProto(90 * 24 * time.Hour),
+		}, 5*time.Second).Result()
+
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to lend book")
+			return
+		}
+
+		if res.(*booklend.CommandStatus).Code != code.Code_OK {
+			log.Warn().Interface("event", res).Msgf("error")
+			return
+		}
+
+		log.Info().Interface("event", res).Msgf("ok")
+
+		res, err = context.RequestFuture(pid, &booklend.ReturnBook{
+			BookId: bookID,
+			Date:   ptypes.TimestampNow(),
+		}, 5*time.Second).Result()
+
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to return book")
+			return
+		}
+
+		if res.(*booklend.CommandStatus).Code != code.Code_OK {
+			log.Warn().Interface("event", res).Msgf("error")
+			return
+		}
+
+		log.Info().Interface("event", res).Msgf("ok")
 	case *actor.Stopping:
 	case *actor.Stopped:
 	case *actor.Restarting:
