43c56050f04c4e13291d6538e2dbb6d5ba73dc65
Chris Camel <camel.christophe@gmail.com>
2020-12-06 13:22:56 +0100

    :sparkles: add streaming capabilities to persistence store

---
+++ internal/persistence/stream.go
@@ -0,0 +1,28 @@
+// Copyright © 2020 Chris Camel <camel.christophe@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package persistence
+
+import (
+	"github.com/AsynkronIT/protoactor-go/actor"
+)
+
+type EventPredicate func(event *EventRecord) bool
+
+type SubscriptionID string
+
+type EventStreamStore interface {
+	// Subscribe does a subscription for events.
+	Subscribe(pid *actor.PID, start *string, predicate EventPredicate) SubscriptionID
+	Unsubscribe(SubscriptionID)
+}
