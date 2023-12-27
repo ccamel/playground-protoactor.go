0e0a1bb05b49bdd1d58a91db267b64e70eb2374e
ccamel <camel.christophe@gmail.com>
2023-12-27 18:06:08 +0100

    :bug: Fix boot sequence order

--- internal/system/core/actor.go
+++ internal/system/core/actor.go
@@ -15,8 +15,8 @@ package core
 
 import (
 	"github.com/asynkron/protoactor-go/actor"
-
 	"github.com/ccamel/playground-protoactor.go/internal/system/core/sys"
+
 	"github.com/ccamel/playground-protoactor.go/internal/system/core/usr"
 )
 
@@ -25,8 +25,8 @@ type Actor struct{}
 func (a *Actor) Receive(context actor.Context) {
 	switch context.Message().(type) {
 	case *actor.Started:
-		_, _ = context.SpawnNamed(actor.PropsFromProducer(sys.New()), "sys")
 		_, _ = context.SpawnNamed(actor.PropsFromProducer(usr.New()), "usr")
+		_, _ = context.SpawnNamed(actor.PropsFromProducer(sys.New()), "sys")
 	case *actor.Stopping:
 	case *actor.Stopped:
 	case *actor.Restarting:
